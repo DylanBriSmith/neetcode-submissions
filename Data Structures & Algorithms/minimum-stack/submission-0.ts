640b5209128b40aedf0b8715c8231b084c0a047c
DylanBriSmith <100657515+DylanBriSmith@users.noreply.github.com>
2026-08-26 10:53:43 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.ts
@@ -0,0 +1,40 @@
+// Need to design a stack class that supports the push, etc operations
+
+
+class MinStack {
+    private stack: number[];
+    
+    constructor() {
+        this.stack = [];
+
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length-1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return Math.min(...this.stack);
+    }
+}
